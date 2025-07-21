3ecaef682fc559669b8f8c9cec44b99e398e5c04
Anderson O. V. Vasconcelos <anderson.vasconcelos@ciss.com.br>
2025-07-21 00:16:16 -0300

    stress test

--- internal/entity/relatorio.go
+++ internal/entity/relatorio.go
@@ -24,16 +24,16 @@ func NewRelatorio() *Relatorio{
 
 func (r *Relatorio) AdicionaResposta(resposta Resposta){
 
-		r.QuantidadeTotalRequestsRealizadas = r.QuantidadeTotalRequestsRealizadas + 1
+	r.QuantidadeTotalRequestsRealizadas = r.QuantidadeTotalRequestsRealizadas + 1
 
-		if (resposta.StatusCode == 200){
-			r.QuantidadeTotalRequestStatus200 = r.QuantidadeTotalRequestStatus200 + 1
-		} else {
-			qt:= r.QuantidadeRequestsErro[resposta.StatusCode]
-			r.QuantidadeRequestsErro[resposta.StatusCode] = qt + 1
-		}
+	if (resposta.StatusCode == 200){
+		r.QuantidadeTotalRequestStatus200 = r.QuantidadeTotalRequestStatus200 + 1
+	} else {
+		qt:= r.QuantidadeRequestsErro[resposta.StatusCode]
+		r.QuantidadeRequestsErro[resposta.StatusCode] = qt + 1
+	}
 
-		r.TempoTotalExecucaoRequest = r.TempoTotalExecucaoRequest + resposta.TempoRequest
+	r.TempoTotalExecucaoRequest = r.TempoTotalExecucaoRequest + resposta.TempoRequest
 }
 
 func (r *Relatorio) Finaliza(){
@@ -43,19 +43,19 @@ func (r *Relatorio) Finaliza(){
 
 func (r *Relatorio) ImprimirRelatorioConsole(){
 	fmt.Printf(
-			"Relatório de execução Stress-Test\n" +
-			"Tempo decorrido = %f\n"+
-			"Tempo Total executando requests = %f(.sec) \n"+
-			"Total de requests realizadas = %d \n" +
-			"Total de requests status 200 = %d \n"+
-			"Erros de execução:\n",
-			r.TempoDecorrido.Seconds(),
-			r.TempoTotalExecucaoRequest.Seconds(),
-			r.QuantidadeTotalRequestsRealizadas,
-			r.QuantidadeTotalRequestStatus200,
-		)
+		"Relatório de execução Stress-Test\n" +
+		"Tempo decorrido = %f\n"+
+		"Tempo Total executando requests = %f(.sec) \n"+
+		"Total de requests realizadas = %d \n" +
+		"Total de requests status 200 = %d \n"+
+		"Erros de execução:\n",
+		r.TempoDecorrido.Seconds(),
+		r.TempoTotalExecucaoRequest.Seconds(),
+		r.QuantidadeTotalRequestsRealizadas,
+		r.QuantidadeTotalRequestStatus200,
+	)
 
-		for key, value := range r.QuantidadeRequestsErro {
-			fmt.Printf("-Status: %d, Total: %d\n", key, value)
-		}
+	for key, value := range r.QuantidadeRequestsErro {
+		fmt.Printf("-Status: %d, Total: %d\n", key, value)
+	}
 }
